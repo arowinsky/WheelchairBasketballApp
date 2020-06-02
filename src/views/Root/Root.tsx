e0274e52b51753817cb05d7d6d534c31c3ce19e6
arowinsky <arowinski96@gmail.com>
2020-06-02 13:35:17 +0200

    Configure: * eslint * prettier * husky

--- src/views/Root/Root.tsx
+++ src/views/Root/Root.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import './Root.module.scss';
 
 const Root = () => {
-  return (
-    <h1>Working!</h1>
-  );
-}
+    return <h1>Working!</h1>;
+};
 
 export default Root;
