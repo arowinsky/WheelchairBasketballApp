8ba1a7e31cc7a9e3b203f9e2676ee9d2c178ba15
arowinsky <arowinski96@gmail.com>
2020-06-03 00:47:09 +0200

    Create stories for reusable Button component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-
-import { Button, Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+import Button from './Button';
 
 storiesOf('Button', module)
-    .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-    .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+    .add('SignUp', () => <Button>Zarejestruj się</Button>)
+    .add('SignIn', () => <Button>Zaloguj się</Button>);
